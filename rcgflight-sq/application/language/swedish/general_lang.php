1106dfe6fcb0cee1be97287a4d5e1457d48f1772
gatecom-jeff <jeff@gatecom.se>
2013-03-13 11:43:08 +0100

    Update general_lang.php

--- rcgflight-sq/application/language/swedish/general_lang.php
+++ rcgflight-sq/application/language/swedish/general_lang.php
@@ -1,8 +1,63 @@
 <?php
-$lang['home'] = 'Startsidan';
-$lang['flight'] = 'Flyg';
-$lang['hotels'] = 'Hotell';
-$lang['car'] = 'Hyrbil';
-$lang['ourterms'] = 'V�ra Villkor';
-$lang['aboutus'] = 'Om Oss';
-$lang['customerservice'] = 'Kundtj�nst';
+/* Menu section */
+$lang['home']   						= 'Startsidan';
+$lang['flight'] 						= 'Flyg';
+$lang['hotels'] 						= 'Hotell';
+$lang['car'] 							= 'Hyrbil';
+$lang['ourterms'] 						= 'Våra Villkor';
+$lang['aboutus'] 						= 'Om Oss';
+$lang['group_bookings'] 				= 'Grupp bokning';
+$lang['terms'] 							= 'Villkor';
+$lang['customer_service'] 				= 'Kundtjänst';
+$lang['login'] 							= 'LOGGA IN';
+$lang['register'] 						= 'REGISTRERA DIG';
+
+/* application/views/flight_search.php */
+$lang['from'] 							= 'Från';
+$lang['to'] 							= 'Till';
+$lang['add_trip'] 						= 'Lägg till resa';
+$lang['trip'] 							= 'Resa';
+$lang['remove_trip'] 					= 'Ta bort resa';
+$lang['departure'] 						= 'Avgång';
+$lang['destination'] 					= 'Destination';
+$lang['return_date'] 					= 'Hemresedatum';
+$lang['adults'] 						= 'Vuxna';
+$lang['children'] 						= 'Barn';
+$lang['one_way'] 						= 'Enkelresa';
+$lang['return'] 						= 'Tur och retur';
+$lang['return_from_another_city'] 		= 'Retur från annan ort';
+$lang['multi_stop'] 					= 'Multi-stop';
+$lang['flexible_search'] 				= 'Flexibelt flyg';
+$lang['direct_flight'] 					= 'Direkt flyg';
+				/* Calendar */
+$lang['sunday'] 						= 'Söndag';
+$lang['monday'] 						= 'Måndag';
+$lang['tuesday'] 						= 'Tisdag';
+$lang['wednesday']						= 'Onsdag';
+$lang['thrusday'] 						= 'Torsdag';
+$lang['friday'] 						= 'Fredag';
+$lang['saturday'] 						= 'Lördag';
+
+$lang['su'] 							= 'Sö';
+$lang['mo'] 							= 'Mo';
+$lang['tu']								= 'Ti';
+$lang['we'] 							= 'On';
+$lang['th'] 							= 'To';
+$lang['fr']								= 'Fr';
+$lang['sa'] 							= 'Lö';
+
+$lang['jan'] 							= 'Januari';
+$lang['feb'] 							= 'Februari';
+$lang['march'] 							= 'Mars';
+$lang['april'] 							= 'April';
+$lang['may'] 							= 'Maj';
+$lang['june'] 							= 'Juni';
+$lang['july'] 							= 'Juli';
+$lang['august'] 						= 'Augusti';
+$lang['spt'] 							= 'September';
+$lang['october'] 						= 'Oktober';
+$lang['nov'] 							= 'November';
+$lang['dec'] 							= 'December';
+
+$lang['last_booked_flights'] 			= 'Nydligen bokade flyg';
+$lang['last_booked_hotels'] 			= 'Nydligen bokade hotell';
